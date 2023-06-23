8d4129a5a6da1e02d286a310ba62f613605a9be0
RevolutionChain Italy <62940168+revolutionchain@users.noreply.github.com>
2023-06-23 13:08:46 +0200

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,23 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function initialSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function mint(uint256 amount) external returns (bool);
-    function grugru(uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-interface IERC721 {
-    function mintNFT(address to) external;
-}
+pragma solidity ^0.8.4;
 
 library SafeMath {
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -33,8 +15,10 @@ library SafeMath {
     }
 }
 
-contract AnoukToken is IERC20 {
+contract AnoukToken  {
     using SafeMath for uint256;
+
+    GrugruNFT public SMasterGRU;
 
     string public name = "Anouk";
     string public symbol = "GRU";
@@ -43,8 +27,10 @@ contract AnoukToken is IERC20 {
     uint256 private _initialSupply = 21000000000 * 10 ** uint256(decimals);
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
-    address private _owner;
-    address private _nftContract;
+    address private _owner;    
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     constructor() {
         _balances[msg.sender] = _initialSupply;
@@ -57,19 +43,19 @@ contract AnoukToken is IERC20 {
         _;
     }
 
-    function totalSupply() public view override returns (uint256) {
+    function totalSupply() public view returns (uint256) {
         return _totalSupply;
     }
 
-    function initialSupply() public view override returns (uint256) {
+    function initialSupply() public view returns (uint256) {
         return _initialSupply;
     }
 
-    function balanceOf(address account) public view override returns (uint256) {
+    function balanceOf(address account) public view returns (uint256) {
         return _balances[account];
     }
 
-    function transfer(address recipient, uint256 amount) public override returns (bool) {
+    function transfer(address recipient, uint256 amount) public returns (bool) {
         require(recipient != address(0), "ERC20: transfer to the zero address");
         require(amount <= _balances[msg.sender], "ERC20: transfer amount exceeds balance");
 
@@ -80,17 +66,17 @@ contract AnoukToken is IERC20 {
         return true;
     }
 
-    function allowance(address owner, address spender) public view override returns (uint256) {
+    function allowance(address owner, address spender) public view returns (uint256) {
         return _allowances[owner][spender];
     }
 
-    function approve(address spender, uint256 amount) public override returns (bool) {
+    function approve(address spender, uint256 amount) public returns (bool) {
         _allowances[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
 
-    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
         require(amount <= _balances[sender], "ERC20: transfer amount exceeds balance");
@@ -104,7 +90,7 @@ contract AnoukToken is IERC20 {
         return true;
     }
 
-    function grugru(uint256 amount) public override returns (bool) {
+    function grugru(uint256 amount) public returns (uint256) {
         require(amount > 0, "Amount must be greater than zero");
         require(_balances[msg.sender] >= amount, "Insufficient balance");
 
@@ -112,15 +98,14 @@ contract AnoukToken is IERC20 {
         _totalSupply = _totalSupply.sub(amount);
         emit Transfer(msg.sender, address(0), amount);
 
-        if (amount > 10000 && _nftContract != address(0)) {
-             IERC721 nftContract = IERC721(_nftContract);
-            nftContract.mintNFT(msg.sender);
+        if (amount > 10000 ) {
+           return SMasterGRU.mintNFT(tx.origin,amount);
         }
 
-        return true;
+        return 0;
     }
 
-    function mint(uint256 amount) public onlyOwner override returns (bool) {
+    function mint(uint256 amount) public onlyOwner returns (bool) {
         _totalSupply = _totalSupply.add(amount);
         _balances[_owner] = _balances[_owner].add(amount);
         emit Transfer(address(0), _owner, amount);
@@ -128,38 +113,38 @@ contract AnoukToken is IERC20 {
         return true;
     }
 
-    function setNFTContract(address nftContract) external onlyOwner {
-        _nftContract = nftContract;
-    }
-}
-
-contract NFTContract is IERC721 {
-    using SafeMath for uint256;
-
-    string public name = "MGRU";
-    string public symbol = "MGRU";
-    string private _tokenUri = "goo.png";
-    mapping(uint256 => address) private _tokenOwners;
-    uint256 private _tokenCount;
-
-    function mintNFT(address to) external override {
-        uint256 tokenId = _tokenCount.add(1);
-        _tokenOwners[tokenId] = to;
-        _tokenCount = _tokenCount.add(1);
-    }
-
-    function ownerOf(uint256 tokenId) public view returns (address) {
-        return _tokenOwners[tokenId];
-    }
-
-    function tokenURI(uint256 tokenId) public view returns (string memory) {
-        require(_tokenOwners[tokenId] != address(0), "Token does not exist");
-        return _tokenUri;
-    }
-
-    function getTokenCount() public view returns (uint256) {
-        return _tokenCount;
+    function ModuleSetEndpoint(address _contract) external onlyOwner {
+        
+       SMasterGRU = GrugruNFT(_contract);
     }
 }
 
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract GrugruNFT is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    address private tokenAddress;
+    string private _imageURL = "grugru.png";
+
+    constructor(address _tokenAddress) ERC721("MGRU", "GRUGRU") {
+        tokenAddress = _tokenAddress;
+    }
+
+    function mintNFT(address to, uint256 amount) external returns (uint256) {
+        require(msg.sender == tokenAddress, "Only the AnoukToken contract can call this function");
+        require(AnoukToken(tx.origin).transferFrom(tx.origin, address(this), amount), "GRU transfer failed");
+        uint256 newTokenId = _tokenIdCounter.current();
+        _safeMint(to, newTokenId);
+        _tokenIdCounter.increment();
+        return newTokenId;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return string(abi.encodePacked(_imageURL));
+    }
+}
